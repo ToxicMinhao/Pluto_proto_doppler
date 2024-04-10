d2922dc5504c3eaa87baf58c3d0295f72de78d66
ToxicMinhao <165098021+ToxicMinhao@users.noreply.github.com>
2024-04-09 23:41:23 -0600

    Update average_speed.py

--- Custom_Python_Block/average_speed.py
+++ Custom_Python_Block/average_speed.py
@@ -2,25 +2,21 @@ import numpy as np
 from gnuradio import gr
 
 
-class doppler_velocity_calc(gr.sync_block):
-    def __init__(self, transmitted_freq=500, speed_of_light=3e8, samp_rate=40000, fft_size = 8192, carrier_freq=3.5e9):
-        gr.sync_block.__init__(self, name="doppler_velocity_calc", in_sig=[np.float32], out_sig=[np.float32])
-        self.transmitted_freq = transmitted_freq
-        self.speed_of_light = speed_of_light
-        self.samp_rate = samp_rate
-        self.fft_size = fft_size
-        self.carrier_freq = carrier_freq
+class average_speed(gr.sync_block): 
+    def __init__(self, target_points = 262144):  
+        gr.sync_block.__init__(self, name='average_speed', in_sig=[np.float32], out_sig=[np.float32])
+        self.target_points = target_points
+
 
     def work(self, input_items, output_items):
         in0 = input_items[0]
         out = output_items[0]
-        part_of_in0 = in0[4050:8192]
-        input_max = np.argmax(part_of_in0)
-        frequency_bin_width = self.samp_rate / self.fft_size
-        if np.max(part_of_in0) < 50:
-            out[:] = 0
-        else: 
-            delta_f = (input_max - (4198-4050)) * frequency_bin_width
-            velocity = (delta_f * self.speed_of_light) / (2*self.carrier_freq)
-            out[:] = velocity
+        accumulated_points = np.array([])
+        while accumulated_points.size < self.target_points:
+            accumulated_points = np.concatenate((accumulated_points, in0))
+        accumulated_points = accumulated_points[:self.target_points]
+        sum_of_points = np.sum(accumulated_points)
+        average = sum_of_points / self.target_points
+        accumulated_points = np.array([])
+        out[:] = average
         return len(output_items[0])
